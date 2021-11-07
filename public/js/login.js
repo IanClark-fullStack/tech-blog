b974e10940383ce7548c0bd3822636e630135aa6
Ian Clark <IanClark2021@u.northwestern.edu>
2021-11-07 16:26:50 -0600

    Finalized Login.js, on sumbit (event handler). Added event handlers for pop-up form functionality, with dynamically replace css classes. - total time: 26:00

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,55 @@
-// Functionality defined as an Even Handler for the SUBMIT 
+// Event Handler for the SUBMIT 
+const loginFormEvent = async (e) => {
+    e.preventDefault();
+    try {
+        // Grab the user input 
+        const inputEmail = document.getElementById('login-email').value.trim();
+        const inputPass = document.getElementById('login-pass').value.trim();
+        // Only if both values exist
+        if (inputEmail && inputPass) {
+            console.log(`TEST for inputs email ${inputEmail} and password ${inputPass}`);
+        // Then send the values to the server
+            // Make a fetch request to userRoutes/login (Which will GET input values, compare hashedPass to Regular and send Flag back of LOGGED_IN)
+            const res = await fetch('/api/users/login', {
+                // The method of the fetch request, is a POST,
+                method: 'POST',
+                // creating new body content in the form of JSON stringified Object {email, pass}
+                body: JSON.stringify({ inputEmail, inputPass }),
+                // send along the content type in the header
+                header: { 'Content-Type': 'application/json' },
+            });
+            if (res.ok) { // After we get a response from userRoutes, 
+                // success = Refresh page. Because we now have a logged_in flag added to the session (sequelize database), that will render the homepage differently. 
+                document.location.replace('/');
+            } else {
+                 // fail = alert
+                 alert(`Yuht-Oh, Login attempt failed`);
+            }
+        }
+    } catch(err) {
+        console.log(err);
+    }
+};
 
-// Collect email and pass values 
-
-// Make a fetch request to userRoutes/login (Which will GET input values, compare hashedPass to Regular and send Flag back of LOGGED_IN)
-    // The method of the fetch request, is a POST, creating new body content in the form of JSON stringified Object {email, pass}
-    // send along the content type in the header
-
-    // After we get a response from userRoutes, 
-        // success = Refresh page. Because we now have a logged_in flag added to the session (sequelize database), that will render the homepage differently. 
-
-        // fail = alert
-//DONE
 
 
+    
+    
+
+   
+const exitFormEvent = (e) => {
+    e.preventDefault();
+    document.getElementById('form-wrap').className('');
+}
+
+const openFormEvent = (e) => {
+    e.preventDefault();
+    // Spread Class List Array into argument. 
+    document.getElementById('form-wrap').classList.remove('overflow-hidden', 'opacity-0').classList.add('block', 'opacity-1');
+}
+
+document.getElementById('open-form').addEventListener('click', openFormEvent);
+document.getElementById('exit-form').addEventListener('click', exitFormEvent);
+
+
+document.getElementById('form-wrap').addEventListener('submit', loginFormEvent);
